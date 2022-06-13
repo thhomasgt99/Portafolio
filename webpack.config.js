4a1e96101034a23405045bac95a650fe85a88c91
Thomas Bernal <thhomasgt99@gmail.com>
2022-06-13 08:51:51 -0500

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     //publicPath: './' //porque ruta arranca| esta line deve estar comentada mientras estemos en desarrollo, en produccion si deve estar activa
     //si esta liena esta activa cuando corramos el proyecto entonces tomara los archivos de las carpetas dist o build, esto es el funcionaiento necesario para produccion pero no para desarrollo por lo tanto deve estar comentada en ese modo
