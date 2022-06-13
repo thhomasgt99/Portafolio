17dbb0b78e4949897ee0db1e4a066ae1671d60c0
Thomas Bernal <thhomasgt99@gmail.com>
2022-06-12 22:00:39 -0500

    Aqui resolvi el dichoso problema del compilado (dist)

--- src/api.js
+++ src/api.js
@@ -6,22 +6,52 @@ import gafas from './images/gafas.jpg'
 
 const api =[
 	{
-		title: 'BatataBit',
+		title: 'BatataBit✔',
 		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
 		img: batata,
 		url: 'https://thhomasgt99.github.io/Batatabit/index.html'
 	},
 	{
-		title: 'Glasses Shop',
+		title: 'Glasses Shop✔',
 		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
 		img: gafas,
 		url: 'https://thhomasgt99.github.io/pagina-gafass/'
 	},
 	{
-		title: 'Yard-Sale',
+		title: 'Yard-Sale❗',
 		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
 		img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAhQAAAIUB4uz/wQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAF1SURBVEiJvdS/SsNQFMfx37kJQts0tZNW2yrFSQUfwZdwE92Ki4PgUwi6CdpNB3HwERwExw6CYAdpS6HRki7a9J/GmlwXFTX9c2pD75ZDzvez3ARgnmTV2kiY1qOSvX3XcoVj7p7gvBQ3a2kpcQpgxnEcpVmvbwXv8he+AHGzliZQBgD9nLcbjTUO0hfoFR8G6QkMinORrgA3zkE8wLDxQcgv4L/xfsg3MGq8FyL8jHdDKFm1NqXECTdulIpsSNP1DCVMqwIgxl0aBlAUxWH9KkY5AsAuANvvMBEhEAodCmM6ck62jEHKfb/iAS2U1QPRVHNpYUcAQHlu8pmIDL8AIlG2VuZLAKAyd1wQjsiVRjAcXnUdJ/Habi9zFlmABK4epiLbn497ADBxk3t6s+3ooF3WLSLIhnfGuxhjuabjAVzIF7+i5FLLCzjiGkBn5DgIUMWZB6jM6vck5TqAgmeJkP87E0L1fDeqqnY0PXzQWkxdfs0+AJRxp6EJFsUfAAAAAElFTkSuQmCC',
 		url: 'https://thhomasgt99.github.io/YARD-SALE/'
+	},
+	{
+		title: '100tifico✔',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
+		img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAhQAAAIUB4uz/wQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAF1SURBVEiJvdS/SsNQFMfx37kJQts0tZNW2yrFSQUfwZdwE92Ki4PgUwi6CdpNB3HwERwExw6CYAdpS6HRki7a9J/GmlwXFTX9c2pD75ZDzvez3ARgnmTV2kiY1qOSvX3XcoVj7p7gvBQ3a2kpcQpgxnEcpVmvbwXv8he+AHGzliZQBgD9nLcbjTUO0hfoFR8G6QkMinORrgA3zkE8wLDxQcgv4L/xfsg3MGq8FyL8jHdDKFm1NqXECTdulIpsSNP1DCVMqwIgxl0aBlAUxWH9KkY5AsAuANvvMBEhEAodCmM6ck62jEHKfb/iAS2U1QPRVHNpYUcAQHlu8pmIDL8AIlG2VuZLAKAyd1wQjsiVRjAcXnUdJ/Habi9zFlmABK4epiLbn497ADBxk3t6s+3ooF3WLSLIhnfGuxhjuabjAVzIF7+i5FLLCzjiGkBn5DgIUMWZB6jM6vck5TqAgmeJkP87E0L1fDeqqnY0PXzQWkxdfs0+AJRxp6EJFsUfAAAAAElFTkSuQmCC',
+		url: 'https://thhomasgt99.github.io/100tifico/'
+	},
+	{
+		title: 'Tasks-app✔',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
+		img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAhQAAAIUB4uz/wQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAF1SURBVEiJvdS/SsNQFMfx37kJQts0tZNW2yrFSQUfwZdwE92Ki4PgUwi6CdpNB3HwERwExw6CYAdpS6HRki7a9J/GmlwXFTX9c2pD75ZDzvez3ARgnmTV2kiY1qOSvX3XcoVj7p7gvBQ3a2kpcQpgxnEcpVmvbwXv8he+AHGzliZQBgD9nLcbjTUO0hfoFR8G6QkMinORrgA3zkE8wLDxQcgv4L/xfsg3MGq8FyL8jHdDKFm1NqXECTdulIpsSNP1DCVMqwIgxl0aBlAUxWH9KkY5AsAuANvvMBEhEAodCmM6ck62jEHKfb/iAS2U1QPRVHNpYUcAQHlu8pmIDL8AIlG2VuZLAKAyd1wQjsiVRjAcXnUdJ/Habi9zFlmABK4epiLbn497ADBxk3t6s+3ooF3WLSLIhnfGuxhjuabjAVzIF7+i5FLLCzjiGkBn5DgIUMWZB6jM6vck5TqAgmeJkP87E0L1fDeqqnY0PXzQWkxdfs0+AJRxp6EJFsUfAAAAAElFTkSuQmCC',
+		url: 'https://thhomasgt99.github.io/Tasks-app/'
+	},
+	{
+		title: 'PlatziConf Merch✔',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
+		img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAhQAAAIUB4uz/wQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAF1SURBVEiJvdS/SsNQFMfx37kJQts0tZNW2yrFSQUfwZdwE92Ki4PgUwi6CdpNB3HwERwExw6CYAdpS6HRki7a9J/GmlwXFTX9c2pD75ZDzvez3ARgnmTV2kiY1qOSvX3XcoVj7p7gvBQ3a2kpcQpgxnEcpVmvbwXv8he+AHGzliZQBgD9nLcbjTUO0hfoFR8G6QkMinORrgA3zkE8wLDxQcgv4L/xfsg3MGq8FyL8jHdDKFm1NqXECTdulIpsSNP1DCVMqwIgxl0aBlAUxWH9KkY5AsAuANvvMBEhEAodCmM6ck62jEHKfb/iAS2U1QPRVHNpYUcAQHlu8pmIDL8AIlG2VuZLAKAyd1wQjsiVRjAcXnUdJ/Habi9zFlmABK4epiLbn497ADBxk3t6s+3ooF3WLSLIhnfGuxhjuabjAVzIF7+i5FLLCzjiGkBn5DgIUMWZB6jM6vck5TqAgmeJkP87E0L1fDeqqnY0PXzQWkxdfs0+AJRxp6EJFsUfAAAAAElFTkSuQmCC',
+		url: 'https://thomb.xyz/'
+	},
+	{
+		title: 'Notes-app fatz curd❌',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
+		img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAhQAAAIUB4uz/wQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAF1SURBVEiJvdS/SsNQFMfx37kJQts0tZNW2yrFSQUfwZdwE92Ki4PgUwi6CdpNB3HwERwExw6CYAdpS6HRki7a9J/GmlwXFTX9c2pD75ZDzvez3ARgnmTV2kiY1qOSvX3XcoVj7p7gvBQ3a2kpcQpgxnEcpVmvbwXv8he+AHGzliZQBgD9nLcbjTUO0hfoFR8G6QkMinORrgA3zkE8wLDxQcgv4L/xfsg3MGq8FyL8jHdDKFm1NqXECTdulIpsSNP1DCVMqwIgxl0aBlAUxWH9KkY5AsAuANvvMBEhEAodCmM6ck62jEHKfb/iAS2U1QPRVHNpYUcAQHlu8pmIDL8AIlG2VuZLAKAyd1wQjsiVRjAcXnUdJ/Habi9zFlmABK4epiLbn497ADBxk3t6s+3ooF3WLSLIhnfGuxhjuabjAVzIF7+i5FLLCzjiGkBn5DgIUMWZB6jM6vck5TqAgmeJkP87E0L1fDeqqnY0PXzQWkxdfs0+AJRxp6EJFsUfAAAAAElFTkSuQmCC',
+		url: 'https://thomb.xyz/'
+	},
+	{
+		title: 'Photo gallery fatz crud❌',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatem, saepe necessitatibus consequuntur dicta repudiandae.',
+		img:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAhQAAAIUB4uz/wQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAF1SURBVEiJvdS/SsNQFMfx37kJQts0tZNW2yrFSQUfwZdwE92Ki4PgUwi6CdpNB3HwERwExw6CYAdpS6HRki7a9J/GmlwXFTX9c2pD75ZDzvez3ARgnmTV2kiY1qOSvX3XcoVj7p7gvBQ3a2kpcQpgxnEcpVmvbwXv8he+AHGzliZQBgD9nLcbjTUO0hfoFR8G6QkMinORrgA3zkE8wLDxQcgv4L/xfsg3MGq8FyL8jHdDKFm1NqXECTdulIpsSNP1DCVMqwIgxl0aBlAUxWH9KkY5AsAuANvvMBEhEAodCmM6ck62jEHKfb/iAS2U1QPRVHNpYUcAQHlu8pmIDL8AIlG2VuZLAKAyd1wQjsiVRjAcXnUdJ/Habi9zFlmABK4epiLbn497ADBxk3t6s+3ooF3WLSLIhnfGuxhjuabjAVzIF7+i5FLLCzjiGkBn5DgIUMWZB6jM6vck5TqAgmeJkP87E0L1fDeqqnY0PXzQWkxdfs0+AJRxp6EJFsUfAAAAAElFTkSuQmCC',
+		url: 'https://thomb.xyz/'
 	}
 ]
 export  default api
